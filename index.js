3c8417e1dbadf17322f0ffd3a056a8b80854cc98
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-03 22:24:27 -0400

    Data structure Object

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newObj = {...driver};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    const newObj = {...driver};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
